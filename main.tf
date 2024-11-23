bbfa199335ae48b222c24a35254aaf2b2defacbf
subhendurs-tech <101401801+subhendurs-tech@users.noreply.github.com>
2024-11-23 21:06:08 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+data "aws_ami" "myami"{
+
+most_recent = true
+
+owners = ["amazon"]
+
+filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+
+}
+
+resource "aws_instance" "myec2" {
+  ami           = data.aws_ami.myami.id
+  instance_type = "t2.micro"
+
+
+
+}
+
+
+resource "aws_vpc" "sl-vpc" {
+  cidr_block       = "10.0.0.0/16"
+   tags = {
+    Name = "sl-vpc"
+  }
+}
